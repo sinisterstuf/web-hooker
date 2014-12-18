0b98ded21a17fadafa4eb25ab5dcd48673f9b41c
Siôn Le Roux <sinisterstuf@gmail.com>
2014-12-18 23:03:55 +0100

    Add created and deleted
    Easy enough with ternary expressions.

--- web-hooker.py
+++ web-hooker.py
@@ -3,6 +3,11 @@
 import json
 import sys
 
+nil="0000000000000000000000000000000000000000"
+
 payload = dict(zip(('before', 'after', 'ref'), sys.stdin.read().split()))
+
+payload['created'] = True if payload['before'] == nil else False
+payload['deleted'] = True if payload['after'] == nil else False
 
 print(json.dumps(payload))
